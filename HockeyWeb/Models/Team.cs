b6ff61cccccdf923f0d27e3d92c623dab3000766
Stefan Holmberg <stefan.holmberg@systementor.se>
2018-12-13 14:08:58 +0100

    team bug

--- HockeyWeb/Models/Team.cs
+++ HockeyWeb/Models/Team.cs
@@ -7,6 +7,10 @@ namespace HockeyWeb.Models
 {
     public class Team
     {
+        public Team()
+        {
+            Players = new List<Player>();
+        }
         public int Id { get; set; }
         public string Namn { get; set; }
         public List<Player> Players { get; set; }
