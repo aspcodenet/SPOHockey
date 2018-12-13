24732c659e80be0333b74f78a6795efc7191a9bf
Stefan Holmberg <stefan.holmberg@systementor.se>
2018-12-13 11:00:58 +0100

    Add project files.

---
+++ HockeyWeb/Controllers/HockeyController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HockeyWeb.Controllers
+{
+    public class HockeyController : Controller
+    {
+
+        public ActionResult Show(int id)
+        {
+            ViewData["HockeyPlayerId"] = id;
+            ViewData["HockeyPlayerName"] = FetchNameFromDatabase(id);
+            return View();
+        }
+
+        private string FetchNameFromDatabase(int id)
+        {
+            if (id == 13) return "Mats Sundin";
+            if (id == 21) return "Peter Forsberg";
+            return "Unknown";
+        }
+
+
+        // GET: Hockey
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
